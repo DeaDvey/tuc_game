9509e4e9922d7932a4ddbb286b47ddf6948fdc34
Max Crawshaw <maxcrawshaw3@hotmail.com>
2023-04-29 14:41:17 +0100

    comments and beutifiying

--- game.cpp
+++ game.cpp
@@ -12,9 +12,7 @@
 #include <sstream>
 
 
-//test
-
-//save data into savefile
+//                       ===save data into savefile===
 void saveToFile(const std::string& fileName, const std::vector<std::pair<std::string, std::string>>& data)
 {
     std::ofstream outFile(fileName);
@@ -29,7 +27,7 @@ void saveToFile(const std::string& fileName, const std::vector<std::pair<std::st
     }
 }
 
-//load variables in data from save file
+//                        ===load variables in data from save file===
 void loadFromFile(const std::string& fileName, std::vector<std::pair<std::string, std::string>>& data) {
     std::ifstream inFile(fileName);
     if (inFile.is_open()) {
@@ -51,7 +49,7 @@ void loadFromFile(const std::string& fileName, std::vector<std::pair<std::string
 }
 
 int main() {
-    //clear screen at the start
+    //clear screen on program start
 	#ifdef _WIN32
         std::system("cls");
     #else
@@ -59,8 +57,9 @@ int main() {
     #endif
 
 
-	//variables//
-	std::vector<std::pair<std::string, std::string>> data {
+	//              ===VARIABLES===
+
+	std::vector<std::pair<std::string, std::string>> data { // main data veriable for character data, this is what gets saved
         {"species", " "},
         {"valid_species", "false"},
         {"first_pronoun", " "},
@@ -70,11 +69,14 @@ int main() {
         {"location", " "}
     };
 
-	std::map<std::string, int> key;
+	std::map<std::string, int> key; //map that stores the above vector
     for (int i = 0; i < data.size(); ++i) {
         key[data[i].first] = i;
     }
-	//variables//
+	//              ===VARIABLES===
+
+
+
 
 	// check if save file exists, and load variables from it into the data vector
 	const std::string saveFileName = "save.txt";
@@ -101,18 +103,24 @@ int main() {
 
 	std::cout << "---Welcome to your TUC adventure!---" << "\n"; //welcome speach said on all loads
 	
-	//character setup state
+	//                          ===character setup state===
 	if (data[key["state"]].second == "setup") {
+        //output
 		std::cout << "I see you have no character, lets make one!" << "\n";
+
+
 		//User inputs species, loops if invalid
 		while (data[key["valid_species"]].second == "false") {
 			std::cout << "Input character species (Overlander/Underlander/Gnawer): " << "\n";
 			std::cin >> data[key["species"]].second;
-			//std::cout << species << "\n";
+
+            //checks if valid
 			if (data[key["species"]].second == "Underlander" || data[key["species"]].second == "Overlander" || data[key["species"]].second == "Gnawer") {
 				std::cout << "Valid species" << "\n";
 				data[key["valid_species"]].second = true;
 			}
+
+			//else it isn't valid so loops
 			else {
 				std::cout << "Invalid species, reinput" << "\n";
 				data[key["valid_species"]].second = false;
@@ -120,35 +128,51 @@ int main() {
 		}
 
 		//get name and pronouns and save to variables
+		//first pronoun
 		std::cout << "Enter your first pronoun, for example they, he or she: " << "\n";
 		std::cin >> data[key["first_pronoun"]].second;
+
+        //second pronoun
 		std::cout << "Enter your second pronoun,, for example them, him or her: " << "\n";
 		std::cin >> data[key["second_pronoun"]].second;
+
+        //name
 		std::cout << "And finally, enter your characters name: " << "\n";
 		std::cin >> data[key["name"]].second;
+
+        //set state to tutorial
 		data[key["state"]].second = "tutorial";
+
 		saveToFile(saveFileName, data); //save data
 	}
 
 
-	//Skip tutorial?
+	//                ===Skip tutorial?===
 	if (data[key["state"]].second == "tutorial") {
 		//Find out if user wants to just skip into main game
 		std::string play_tutorial;
 		std::cout << "Play the tutorial?(y/n) " << "\n";
 		std::cin >> play_tutorial;
+
+        //if the user wants to skip, asign state as main
 		if (play_tutorial == "n") {
 			std::cout << "Ok, that's fine.  You will be placed in your starting location" << "\n";
 			data[key["state"]].second = "main";
 
 		}
+
+		//if use wants to contnue with tutorial, dont change state
 		else if (play_tutorial == "y") {
 			std::cout << "Welcome to the tutorial" << "\n";
 
 		}
+
+
 		saveToFile(saveFileName, data); //save data
 	}
-	//Tutorial state
+
+
+	//                          ===Tutorial state===
 	if (data[key["state"]].second == "tutorial") {
 
 
@@ -177,12 +201,17 @@ int main() {
 		saveToFile(saveFileName, data); //save data
 	}
 
-	//main game
+
+
+
+
+
+	//                                   ===main game===
 	if (data[key["state"]].second == "main") {
 		std::cout << "Main game not yet made :(" << "\n";
 	}
 
-	//else
+	//else check, if state is invalid
 	else if (data[key["state"]].second != "setup" && data[key["state"]].second != "tutorial" && data[key["state"]].second != "main") { //lets up know if state has a corrupted value
 			std::cout << "state is messed up bruh" << "\n";
 			return 1;
