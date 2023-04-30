c90d184a533c5dcfbd4b6486ffe27d23159d8baf
Max Crawshaw <maxcrawshaw3@hotmail.com>
2023-04-30 18:13:42 +0100

    bash script comments

--- compile.sh
+++ compile.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+#Bash file that compiles the CPP code.
+#Not nessacary, but useful as there are a lot of files
 
-g++ funtions.cpp game.cpp main.cpp tutorial.cpp read_input.cpp -o game
+
+g++ funtions.cpp game.cpp main.cpp tutorial.cpp read_input.cpp -o run
+
+
+
+
