c04920c3545b465642fb92be7a57611587c7f3ba
Max Crawshaw <maxcrawshaw3@hotmail.com>
2023-05-03 22:41:10 +0100

    added message in compiler.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Bash file that compiles the CPP code.
 #Not nessacary, but useful as there are a lot of files
+#if making new C++ files, please add them here  :D
 
 
 g++ map_descriptions.cpp display_map.cpp map_defenitions.cpp map.cpp funtions.cpp character_setup.cpp game.cpp main.cpp tutorial.cpp read_input.cpp -o run
