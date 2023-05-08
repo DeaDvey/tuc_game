7658aae5884e606dfeeb9c8a3c3b6ef18b905d1d
DeaDvey <maxcrawshaw3@hotmail.com>
2023-05-08 22:54:22 +0100

    push before sleep ._.zZZZZ

--- inventory.cpp
+++ inventory.cpp
@@ -10,8 +10,9 @@
 #include <unordered_map>
 #include <vector>
 #include <sstream>
-#include <cctype>
+#include <sstype>
 #include <array>
+#include <sstream>
 #include "header.h"
 
 std::string check_inventory(std::vector<std::pair<std::string, std::string>>& data, const std::string& saveFileName) {
@@ -22,6 +23,19 @@ std::string check_inventory(std::vector<std::pair<std::string, std::string>>& da
 
 std::string add_to_inventory(std::vector<std::pair<std::string, std::string>>& data, const std::string& saveFileName) {
 
+  //data key for accessing elements
+  std::map<std::string, int> key; //map that stores the above vector
+  for (int i = 0; i < data.size(); ++i) {
+      key[data[i].first] = i;
+  }
+
+  std::istringstream iss(input);
+  std::vector<std::string> words{std::istream_iterator<std::string>{iss},
+                                    std::istream_iterator<std::string>{}};
+    for (const auto& word : words) {
+        std::cout << word << '\n';
+    }
+    
   
   return "done";
 }
